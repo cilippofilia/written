5034a42dacacdf075b957de50bb78ecca6bd8bc0
Filippo Cilia <filippocarlocilia@gmail.com>
2025-09-03 19:16:01 +0100

    Initial Commit

---
+++ written/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  written
+//
+//  Created by Filippo Cilia on 03/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
